f0fe6d6c374c29ac02a2a462ee86ef7f70502885
mbernardo23 <145507702+mbernardo23@users.noreply.github.com>
2023-10-10 20:40:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Altera o texto do título
+document.querySelector("h1").textContent = "Explorando o Mundo Tecnológico";
+// Adiciona um link para o GitHub
+document.querySelector("footer").innerHTML = <a href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>;
