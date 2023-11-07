b5e25288b5997625a037ffcb8edc7bfbb5b676b0
mbernardo23 <145507702+mbernardo23@users.noreply.github.com>
2023-11-07 19:58:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// Altera o texto do título
-document.querySelector("h1").textContent = "Explorando o Mundo Tecnológico";
-// Adiciona um link para o GitHub
-document.querySelector("footer").innerHTML = <a href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>;
+
