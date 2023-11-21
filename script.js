a90b17c080458e08c3517baeb11a9ad809dde88a
mbernardo23 <145507702+mbernardo23@users.noreply.github.com>
2023-11-21 01:59:50 -0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('nav a');
+  
+    navLinks.forEach(link => {
+      link.addEventListener('click', smoothScroll);
+    });
+  
+    function smoothScroll(e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      const offset = targetElement.offsetTop - header.offsetHeight;
+  
+      window.scroll({
+        top: offset,
+        behavior: 'smooth'
+      });
+    }
+  });
+  
